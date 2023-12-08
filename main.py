40fb128d7048f140390295a09c7e3ed5efc90bcb
Narla-Venkata-Anand-Sai-Kumar <venkatnarla0@gmail.com>
2023-12-08 13:19:08 +0530

    updated

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from utils.encodes import find_encodings
 from Algostructured_new import *
 from utils.encodes import *
+from utils.attendace_write import *
+from utils.sheduler import *
 
 known_face_encodings, known_face_names = dry_run()
 
-a=Find_attend(known_face_encodings,known_face_names)
+# a=Find_attend(known_face_encodings,known_face_names)
 
 
-def execute_functions():
-    current_time = datetime.now().time()
-    current_minute = current_time.minute
-    final_list = {}
-    if current_minute < 30:
-        first_half = Find_attend(known_face_encodings, known_face_names)
-    elif current_minute >= 31 & current_minute <= 50:
-        second_half = Find_attend(known_face_encodings, known_face_names)
-    
-    for i in second_half:
-        if second_half[i] not in first_half:
-            pass
-        else:
-            first_half[i].append(second_half[i])
-    return first_half
+attendance_data = execute_functions(known_face_encodings,known_face_names)
+
+
+student_data_file = 'attendance/input.csv'
+output_file = 'attendance/output.csv'
+student_data = read_student_data(student_data_file)
+
+write_to_csv(attendance_data, student_data, output_file)
--- utils/attendace_write.py
+++ utils/attendace_write.py
@@ -1,19 +1,52 @@
-import pandas as pd
+import csv
+from datetime import datetime
 
-def markAttendance(name):
-    if 'Unknown' in name:
-        name.remove('Unknown')
-    p={'kumar':['99210041514'],'Karthik':['99210041518']}
-    l2=[]
-    l3=[]
-    for i in name:
-       if i in p:
-           l2.append(p[i][0])
-        #    l3.append(p[i][1])
-       else:
-           l2.append('UnKnown')
-        #    l3.append('Unknown')
-    p={'name':name,'Reg no':l2}
-    n = pd.DataFrame(p)
-    n.to_csv("attx.csv")
-                
+def time_diff_in_minutes(start_time, end_time):
+    format_str = "%H:%M:%S"
+    start = datetime.strptime(start_time, format_str)
+    end = datetime.strptime(end_time, format_str)
+    diff_seconds = (end - start).total_seconds()
+    diff_minutes = diff_seconds / 60
+    return int(diff_minutes)
+
+def read_student_data(file_path):
+    student_data = []
+    with open(file_path, 'r', newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # Remove the BOM character if present in column names
+            corrected_row = {key.replace('\ufeff', ''): value for key, value in row.items()}
+            student_data.append(corrected_row)
+    print(student_data)
+    return student_data
+
+def write_to_csv(attendance_data, student_data, output_file):
+    student_dict = {student['rollno']: student for student in student_data}
+
+    with open(output_file, 'w', newline='') as csvfile:
+        fieldnames = ['Name','Roll No', 'InTime', 'OutTime', 'Email', 'TimeDifference']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for roll_no, times in attendance_data.items():
+            name = student_dict[roll_no]['name']
+            email = student_dict[roll_no]['email']
+            intime = times[0]
+            outtime = times[1]
+            difference = None
+            if intime and outtime:
+                difference = time_diff_in_minutes(intime, outtime)
+
+            writer.writerow({
+                'Name': roll_no,
+                'Roll No': name,
+                'InTime': intime,
+                'OutTime': outtime,
+                'Email': email,
+                'TimeDifference': difference if difference is not None else 'Not Available'
+            })
+
+# Example usage:
+
+output_file = 'attendance/output.csv'
+write_to_csv(attendance_data, student_data, output_file)
