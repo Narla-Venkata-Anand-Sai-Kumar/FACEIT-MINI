6b1c80357434b31db6394e9894113d1e28b323ee
Narla-Venkata-Anand-Sai-Kumar <venkatnarla0@gmail.com>
2023-12-19 10:32:45 +0530

    updated

--- utils/email_automation.py
+++ utils/email_automation.py
@@ -20,8 +20,8 @@ def mailer(EMAIL_TO,name):
     msg = MIMEMultipart()
     msg['From'] = EMAIL_FROM
     msg['To'] = EMAIL_TO
-    msg['Subject'] = 'KARE-OSS FACEIT TEST !'
-    text = MIMEText(f"Dear {name},\n\nWe would like to express our sincere gratitude for your participation in the KARE OSS Kaggle Code Competition held on 19th June. Your contribution and dedication were instrumental in making the competition a resounding success. We were impressed by your exceptional skills and problem-solving abilities, which truly elevated the standard of the event. Thank you for showcasing the spirit of collaboration and innovation.\n\nAs a token of appreciation, we have attached a participation certificate to this email. It serves as a memento of your outstanding performance and demonstrates our recognition of your hard work. We hope you cherish this certificate as a testament to your remarkable skills.\n\nThe success of this competition has inspired us to organize more such events in the future. We eagerly await the opportunity to see you participate in our upcoming events.\n\nBest regards,\nN V ANAND SAI (PRESIDENT,OSS),\nB KUMAR SRINIVAS (VICE-PRESIDENT,OSS),\nB KARTHIK TARAKA SAI (SECRETARY,OSS),\n& TEAM KARE-OSS.")
+    msg['Subject'] = 'KARE-OSS FACEIT VERSION 1 !'
+    text = MIMEText(f"Dear {name},\n\nYour attendance has been recorded Successfully.\n\nThanks & Regards,\nKARE-OSS Team")
     msg.attach(text)
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
         smtp.ehlo()
@@ -31,4 +31,8 @@ def mailer(EMAIL_TO,name):
 
 
 df = pd.read_csv('attendance/output.csv')
-print(df)
+emails,names,in_time,out_time,total,regno = df['Email'].tolist(),df['Name'].tolist(),df['InTime'].tolist(),df['OutTime'].tolist(),df['TimeDifference'].tolist(),df['Roll No'].tolist()
+
+for i in range(len(emails)):
+    mailer(emails[i],names[i])
+    print(f"Mail sent to {names[i]}")
